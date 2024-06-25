ff86e404342b695fb7d75e2755a9535e6f1aefe7
Murad <yusifovmurad1@gmail.com>
2024-06-25 16:08:24 +0400

    String methods

---
+++ main.py
@@ -0,0 +1,20 @@
+# user_name = "Murad"
+# age = 17
+# is_admin = False
+
+# print(f"Hello, {user_name}. Your age is {age}. Is admin status : {is_admin}")
+
+city = "Baku is capital of Azerbaijan"
+age = "25"
+
+# print(len(city))
+# print(city.find("k"))
+# print(city.upper())
+# print(city.lower())
+# print(city.title())
+# print(city.isdigit())
+# print(age.isdigit())
+# print(city.isalpha())
+# print(city.count("i"))
+# print(city.replace("i", "s"))
+# print(city[0:4]*3)
