aa4cec8efd482eea78d9c2f6c504fda15fef1d5f
Murad <yusifovmurad1@gmail.com>
2024-08-20 21:54:06 +0400

    Traning

---
+++ training.py
@@ -0,0 +1,41 @@
+# name = "Murad"
+# age = 17
+# hobby = "coding"
+
+# movies = ["The Godfather", "The Dark Knight", "The Prestige"]
+# movies.append("Inception")
+# print(movies[1])
+
+# try:
+#     age = int(input("Enter your age: "))
+#     if age < 18:
+#         print("You are minor!")
+#     elif age >= 18 and age < 65:
+#         print("You are an adult!")
+#     else:
+#         print("You are an elder!")
+# except ValueError:     
+#     print("Invalid input!")
+    
+
+# def greet(name: str) -> None:
+#     print(f"My name is {name}")
+    
+# greet("Murad")
+
+# def add (a: int, b: int) -> int:
+#     return a + b
+
+# print(add(1, 2))
+
+# def sumNumbers(list: list) -> int:
+#     sum = 0
+#     for number in list:
+#         sum += number
+#     return sum
+
+# print(sumNumbers([1, 2, 3, 4, 5]))
+
+# hello = lambda name: print(f"Hello {name}!")
+
+# hello("Murad")
