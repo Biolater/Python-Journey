3ae768fa5c720ce81e7979373dfee82bf91b6422
Murad <yusifovmurad1@gmail.com>
2024-08-21 10:12:23 +0400

    Training fundementals

--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+import random
+
 # name = "Murad"
 # age = 17
 # hobby = "coding"
@@ -39,3 +41,65 @@
 # hello = lambda name: print(f"Hello {name}!")
 
 # hello("Murad")
+
+
+# person = {
+#     "name": "Murad",
+#     "age": 17,
+#     "hobby": "coding",
+#     "city": "Baku"
+# }
+
+# print(person.get("name"))
+# person.update({"city": "Istanbul"})
+# person.update({"favorite_color": "blue"})
+# print(person)
+
+# sentence = "Python is fun!"
+# print(sentence.title())
+# if "Python" in sentence:
+#     print("Yes")
+# else:
+#     print("No")
+# print(len(sentence))
+
+# print(sentence[:4])
+# print(sentence.split(" ")[len(sentence.split(" ")) - 1])
+# modified_sentence = sentence.replace("fun", "awesome")
+
+# print(modified_sentence)
+
+
+# for i in range(1,6):
+#     for j in range (1, 6):
+#         product = i * j
+#         print(f"{i} * {j} = {product}")
+        
+    
+        
+# random_n = random.randint(1,11)
+
+# while True:
+#     try:
+#         number = int(input("Enter a number: "))
+#         if number == random_n:
+#             print("You guessed correctly!")
+#             break
+#         elif number < random_n:
+#             print("Higher")
+#             continue
+#         else:
+#             print("Lower")
+#             continue
+#     except ValueError:
+#         print("Invalid input! Please enter a number.")
+#         continue
+
+
+# numbers = [n for n in range(1,11)]
+# squares = [n*2 for n in numbers]
+# evens = [n for n in numbers if n % 2 == 0]
+
+# print(numbers)
+# print(squares)
+# print(evens)
