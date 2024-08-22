2a67813060fa4c9a734baadfe8e39dd9ea614988
Murad <yusifovmurad1@gmail.com>
2024-08-22 11:18:04 +0400

    Training

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # name = "Murad"
 # age = 17
@@ -103,3 +104,37 @@ import random
 # print(numbers)
 # print(squares)
 # print(evens)
+
+# with open("new.txt", "r") as text_file:
+#     print(text_file.read())
+
+# def write_user_info_to_file():
+#     try:
+#         name = input("Please enter your name: ")
+#         age = int(input("Please enter your name: "))
+#         if len(name.strip()) == 0:
+#             raise ValueError("Name cannot be empty.")
+#         with open("user_info.txt", "w") as user_info_file:
+#             user_info_file.write(f"Name: {name}\n")
+#             user_info_file.write(f"Age: {age}\n")
+#         print("User information has been saved to user_info.txt!")
+
+#     except ValueError:
+#         print("Invalid age entered. Please enter a number.")
+
+# write_user_info_to_file()
+
+
+# class Dog:
+#     def __init__(self, name, breed) -> None:
+#         self.name = name
+#         self.breed = breed
+        
+#     def bark(self):
+#         print(f"{self.name}: Woof! Woof!")
+    
+    
+
+# pitbull = Dog("pitbull", "wild")
+
+pitbull.bark()
